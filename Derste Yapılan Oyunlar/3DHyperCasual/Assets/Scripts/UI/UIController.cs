321ef22a59c259a47531fb9eeecc2631eba48de4
raziyedemiralay <1444sezgin@gmail.com>
2023-10-26 17:55:21 +0300

    Updates

---
+++ Derste Yapılan Oyunlar/3DHyperCasual/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // This is needed to load scenes
+
+public class UIController : MonoBehaviour
+{
+
+
+    public void PlayGame() // This function is called when the Play button is clicked
+    {
+        SceneManager.LoadScene("Level 1"); // Load the Level 1 scene
+    }
+}
